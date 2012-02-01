daf04b8a4573facd8cafedff6c625d81a6dc62e2
UCHIDA Masaru <parallelopipedon@gmail.com>
2012-02-01 23:33:37 +0900

    unity added

---
+++ unity/LedDriver/LedDriverTest.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+#include "unity_fixture.h"
+#include "LedDriver.h"
+
+TEST_GROUP(LedDriver);
+
+static uint16_t virtualLeds;
+
+TEST_SETUP(LedDriver){
+	LedDriver_Create(&virtualLeds);
+}
+
+TEST_TEAR_DOWN(LedDriver){
+
+}
+
+TEST(LedDriver, LedsOffAfterCreate){
+	uint16_t virtualLeds = 0xffff;
+	LedDriver_Create(&virtualLeds);
+	TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+}
+
+TEST(LedDriver, TurnOnLedOne){
+	LedDriver_TurnOn(1);
+	TEST_ASSERT_EQUAL_HEX16(1, virtualLeds);
+}
+
+TEST(LedDriver, TurnOffLedOne){
+	LedDriver_TurnOn(1);
+	LedDriver_TurnOff(1);
+	TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+}
