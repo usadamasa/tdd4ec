70835338868d8041109206805ad3ae381b1afcdb
UCHIDA Masaru <parallelopipedon@gmail.com>
2012-02-26 15:10:42 +0900

    Refactoring LedDriver_TurnOn

--- src/LedDriver/LedDriver.c
+++ src/LedDriver/LedDriver.c
@@ -11,10 +11,16 @@ void LedDriver_Destroy(void){
 
 }
 
+static uint16_t convertLedNumberToBit(int ledNumber)
+{
+	return 1 << (ledNumber - 1);
+}
+
 void LedDriver_TurnOn(int ledNumber){
-	*ledsAddress |= 1 << (ledNumber - 1);
+	*ledsAddress |= convertLedNumberToBit(ledNumber);
 }
 
 void LedDriver_TurnOff(int ledNumber){
 	*ledsAddress = 0;
 }
+
